922a25511924a137c317423477bd225d2970a801
Afzal Koukar <afzalkoukar@gmail.com>
2024-02-21 13:38:27 +0530

    learned redux-toolkit basics

---
+++ todo-redux-toolkit/src/App.jsx
@@ -0,0 +1,19 @@
+import Todo from './components/Todo'
+
+import './App.css'
+import AddTodo from './components/AddTodo'
+
+function App() {
+  
+
+  return (
+    <><h1>Todo App</h1>
+    
+    <AddTodo/>
+    <Todo/>
+
+    </>
+  )
+}
+
+export default App
